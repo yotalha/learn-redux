47c259c06fad2b50e1deae874f30e9e51972ff87
yotalha <talha.coder9@gmail.com>
2021-06-09 02:57:40 +0500

    dev ed video complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import React from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import {increment, decrement} from './actions';
 
 
 function App() {
+  const counter = useSelector(state => state.counter)
+  const isLogged = useSelector(state => state.isLogged)
+  const dispatch = useDispatch();
+
+
+
   return (
     <div>
-      <h1>sup nigero</h1>
+      <h1>Counter {counter}</h1>
+      <button onClick={() => dispatch(increment(4))}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      
+      {isLogged ? <h3>Valuable information i cant see without being logged in first</h3> : ''}
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {createStore} from 'redux';
+import allReducer from './reducers'
+import {Provider} from 'react-redux';
+
+const store = createStore(allReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 
 //STORE -> GLOBALIZED STATE
@@ -42,9 +46,9 @@ import {createStore} from 'redux';
 // store.dispatch(decrement());
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
